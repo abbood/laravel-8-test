d2d639b43c2a0d6f27802a4003fc2d2d959b1ea2
abdulah bakhach <abdullah.bakhach@gmail.com>
2022-12-22 13:43:27 +0200

    minor fixes to match repo

--- app/Models/TestModel.php
+++ app/Models/TestModel.php
@@ -3,22 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
-class TestModel extends Model
+class TestModel extends BaseModel
 {
     use HasFactory;
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::saving(function ($model) {
-            if (is_null($model->getKey())) {
-                $id = rand(1, 999999999999999);
-                $model->setAttribute($model->getKeyName(), $id);
-            }
-        });
-    }
-
 }
+
